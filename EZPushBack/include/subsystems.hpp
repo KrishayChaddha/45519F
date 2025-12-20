c05bcbcf8ae0f82bfdbb4aea915891310488a789
ComSciBR <147848208+ComSciBR@users.noreply.github.com>
2025-12-20 02:41:29 -0700

    Add existing file

---
+++ EZPushBack/include/subsystems.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+ inline pros::Motor intake(-15, pros::MotorGears::blue);
+ inline pros::Motor outtake(13, pros::MotorGears::green, pros::MotorUnits::degrees);
+ inline ez::Piston funnel('H');
+ inline ez::Piston descore('G');
+ inline ez::Piston matchload('F');
+// inline pros::adi::DigitalIn limit_switch('A');
